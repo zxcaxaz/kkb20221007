9972298e0635a1107393daba02c1170550cef696
zxcaxaz <gam98846@gmail.com>
2022-10-07 15:06:58 +0900

    메인 ui 2 작업 및 회원 name 추가

--- webstudy99-kkb221007/src/main/java/org/kosta/projectname/model/MemberVO.java
+++ webstudy99-kkb221007/src/main/java/org/kosta/projectname/model/MemberVO.java
@@ -2,6 +2,7 @@ package org.kosta.projectname.model;
 
 public class MemberVO {
 	private String id;
+	private String name;
 
 	public String getId() {
 		return id;
@@ -9,6 +10,14 @@ public class MemberVO {
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
--- webstudy99-kkb221007/src/main/webapp/index.jsp
+++ webstudy99-kkb221007/src/main/webapp/index.jsp
@@ -18,7 +18,7 @@
 </head>
 <body>
 <div class="container pt-3">
-	main1 	ui 작업 
+	main1 	ui 작업 ui 작업 ver2
 </div>
 </body>
 </html>
